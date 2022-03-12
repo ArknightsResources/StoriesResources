224511d7f3cd490b6076a3ba9035bae4f2594930
Baka632 <stevemc123456@outlook.com>
2022-03-12 22:18:06 +0800

    将资源项目和与资源无关的代码拆分

---
+++ ResourceHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using StoryResources = ArknightsResources.Stories.Resources.Properties.Resources;
+
+namespace ArknightsResources.Stories.Resources
+{
+    /// <summary>
+    /// 为ArknightsResources.Stories.Resources的资源访问提供帮助的类
+    /// </summary>
+    public static class ResourceHelper
+    {
+        /// <summary>
+        /// 通过剧情文件的代号获取该剧情的原始剧情文本
+        /// </summary>
+        /// <param name="codename">剧情文件的代号</param>
+        /// <param name="cultureInfo"></param>
+        /// <returns>原始剧情文本</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static string GetStoryText(string codename, CultureInfo cultureInfo)
+        {
+            if (string.IsNullOrWhiteSpace(codename))
+            {
+                throw new ArgumentException($"“{nameof(codename)}”不能为 null 或空白。", nameof(codename));
+            }
+
+            try
+            {
+                string text = StoryResources.ResourceManager.GetString(codename, cultureInfo);
+                return text;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException($"使用给定的参数\"{codename}\"时找不到资源", ex);
+            }
+        }
+    }
+}
